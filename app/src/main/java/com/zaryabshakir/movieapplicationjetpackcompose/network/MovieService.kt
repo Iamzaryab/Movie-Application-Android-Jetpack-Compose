8789e2dd2101e96ee06bab9192fb789d63751fce
Iamzaryab <zaryabdev@gmail.com>
2022-09-26 16:01:14 +0500

    Retrofit MovieService

---
+++ app/src/main/java/com/zaryabshakir/movieapplicationjetpackcompose/network/MovieService.kt
@@ -0,0 +1,32 @@
+package com.zaryabshakir.movieapplicationjetpackcompose.network
+
+import com.zaryabshakir.movieapplicationjetpackcompose.network.model.MovieDetailDto
+import com.zaryabshakir.movieapplicationjetpackcompose.network.responses.GenresResponse
+import com.zaryabshakir.movieapplicationjetpackcompose.network.responses.MoviesResponse
+import retrofit2.http.Path
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieService {
+
+
+    @GET("movie/{type}")
+    suspend fun getMovies(
+        @Path("type") type: String,
+        @Query("page") page: Int,
+        @Query("api_key") key: String
+    ): MoviesResponse
+
+
+    @GET("genre/movie/list")
+    suspend fun genre(
+        @Query("api_key") key: String
+    ): GenresResponse
+
+    @GET("movie/{id}")
+    suspend fun get(
+        @Path("id") id: Int,
+        @Query("api_key") key: String
+    ): MovieDetailDto
+
+}
