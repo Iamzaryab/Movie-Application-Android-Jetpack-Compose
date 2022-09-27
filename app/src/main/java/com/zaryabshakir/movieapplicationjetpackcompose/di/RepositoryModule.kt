5ad1ca023f5a0b9da055e62d231a45656145fac5
Iamzaryab <zaryabdev@gmail.com>
2022-09-27 23:01:21 +0500

    Repository Module Implemented

---
+++ app/src/main/java/com/zaryabshakir/movieapplicationjetpackcompose/di/RepositoryModule.kt
@@ -0,0 +1,31 @@
+package com.zaryabshakir.movieapplicationjetpackcompose.di
+
+import com.zaryabshakir.movieapplicationjetpackcompose.network.MovieService
+import com.zaryabshakir.movieapplicationjetpackcompose.network.model.dtoMapper.GenreDtoMapper
+import com.zaryabshakir.movieapplicationjetpackcompose.network.model.dtoMapper.MovieDetailDtoMapper
+import com.zaryabshakir.movieapplicationjetpackcompose.network.model.dtoMapper.MovieDtoMapper
+import com.zaryabshakir.movieapplicationjetpackcompose.repository.MovieRepository
+import com.zaryabshakir.movieapplicationjetpackcompose.repository.MovieRepository_Impl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun providesMovieRepository(
+        movieService: MovieService,
+        movieDtoMapper: MovieDtoMapper,
+        movieDetailDtoMapper: MovieDetailDtoMapper,
+        genreDtoMapper: GenreDtoMapper
+    ): MovieRepository {
+        return MovieRepository_Impl(
+            movieService, movieDtoMapper, movieDetailDtoMapper, genreDtoMapper,
+        )
+    }
+}
