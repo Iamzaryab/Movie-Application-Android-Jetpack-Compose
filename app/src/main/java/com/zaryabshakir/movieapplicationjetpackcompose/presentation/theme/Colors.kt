45608dd245975c2f3e38a40f3b4c14290e00bd3a
Iamzaryab <zaryabdev@gmail.com>
2022-09-28 22:46:44 +0500

    Colors added

---
+++ app/src/main/java/com/zaryabshakir/movieapplicationjetpackcompose/presentation/theme/Colors.kt
@@ -0,0 +1,10 @@
+package com.zaryabshakir.movieapplicationjetpackcompose.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val RatingIconColor = Color(0xFFFFC319)
+val RatingTextColor = Color(0xFF9C9C9C)
+val SeeMoreBorderColor = Color(0xFFE5E4EA)
+val SeeMoreTextColor = Color(0xFFAAA9B1)
+val GenreChipTextColor = Color(0xFF88A4E8)
+val GenreChipBg = Color(0xFFDBE3FF)
