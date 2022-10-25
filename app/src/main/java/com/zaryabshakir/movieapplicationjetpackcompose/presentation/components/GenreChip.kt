550e36091e8ecccf01efe62c7f064368436ec35a
Iamzaryab <zaryabdev@gmail.com>
2022-10-25 21:49:26 +0500

    theme update

--- app/src/main/java/com/zaryabshakir/movieapplicationjetpackcompose/presentation/components/GenreChip.kt
+++ app/src/main/java/com/zaryabshakir/movieapplicationjetpackcompose/presentation/components/GenreChip.kt
@@ -3,35 +3,30 @@ package com.zaryabshakir.movieapplicationjetpackcompose.presentation.components
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.zaryabshakir.movieapplicationjetpackcompose.domain.model.Genre
-import com.zaryabshakir.movieapplicationjetpackcompose.presentation.theme.GenreChipBg
-import com.zaryabshakir.movieapplicationjetpackcompose.presentation.theme.GenreChipTextColor
-import com.zaryabshakir.movieapplicationjetpackcompose.presentation.theme.SeeMoreBorderColor
-import com.zaryabshakir.movieapplicationjetpackcompose.presentation.theme.SeeMoreTextColor
+import java.util.*
 
 @Composable
 fun GenreChip(genreId: Int, genres: List<Genre>) {
     Surface(
         shape = RoundedCornerShape(18.dp),
-        border = BorderStroke(1.dp, GenreChipBg),
-        color = GenreChipBg,
-        modifier = Modifier.padding(end = 8.dp)
+        border = BorderStroke(1.dp, MaterialTheme.colors.secondary),
+        color = MaterialTheme.colors.secondary,
+        modifier = Modifier
+            .padding(end = 8.dp)
     ) {
 
         Text(
-            text = genres.find { it.id == genreId }?.name.toString(),
-            style = TextStyle(
-                color = GenreChipTextColor,
-                fontSize = 12.sp
-            ),
-            modifier = Modifier.padding(top = 2.dp, bottom = 2.dp, start = 10.dp, end = 10.dp)
+            text = genres.find { it.id == genreId }?.name.toString().uppercase(Locale.getDefault()),
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.onSecondary,
+            modifier = Modifier.padding(top = 4.dp, bottom = 4.dp, start = 10.dp, end = 10.dp)
         )
     }
 }
