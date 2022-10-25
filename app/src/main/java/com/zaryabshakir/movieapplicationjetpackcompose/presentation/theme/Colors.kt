1a33337d3e0e6900ae15ade8fce8b6aa2eadb3b7
Iamzaryab <zaryabdev@gmail.com>
2022-10-25 21:48:27 +0500

    Colors

--- app/src/main/java/com/zaryabshakir/movieapplicationjetpackcompose/presentation/theme/Colors.kt
+++ app/src/main/java/com/zaryabshakir/movieapplicationjetpackcompose/presentation/theme/Colors.kt
@@ -2,9 +2,26 @@ package com.zaryabshakir.movieapplicationjetpackcompose.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
+
+//Light Theme Colors
+val primaryLight = Color(0xFF110E47)
+val primaryVariantLight = Color(0xFF2D2F73)
+val secondaryLight = Color(0xFFDBE3FF)
+val secondaryVariantLight = Color(0xFF9C9C9C)
+val onSecondaryLight = Color(0xFF88A4E8)
+//Dark Theme Colors
+
+
+//Error
+val error = Color(0xFFB00020)
 val RatingIconColor = Color(0xFFFFC319)
-val RatingTextColor = Color(0xFF9C9C9C)
+val subtitleColor = Color(0xFF9C9C9C)
 val SeeMoreBorderColor = Color(0xFFE5E4EA)
 val SeeMoreTextColor = Color(0xFFAAA9B1)
-val GenreChipTextColor = Color(0xFF88A4E8)
-val GenreChipBg = Color(0xFFDBE3FF)
+
+//Shimmer Gradient Color
+val ShimmerColors = listOf(
+    Color.LightGray.copy(alpha = 0.9f),
+    Color.LightGray.copy(alpha = 0.2f),
+    Color.LightGray.copy(alpha = 0.9f)
+)
