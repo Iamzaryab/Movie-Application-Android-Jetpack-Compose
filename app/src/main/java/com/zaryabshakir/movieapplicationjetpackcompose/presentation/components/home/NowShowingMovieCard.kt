02d6fd865ca10889ab7493e8f867db0e7c1fa329
Iamzaryab <zaryabdev@gmail.com>
2022-10-26 19:44:48 +0500

    updates

--- app/src/main/java/com/zaryabshakir/movieapplicationjetpackcompose/presentation/components/home/NowShowingMovieCard.kt
+++ app/src/main/java/com/zaryabshakir/movieapplicationjetpackcompose/presentation/components/home/NowShowingMovieCard.kt
@@ -1,23 +1,25 @@
-package com.zaryabshakir.movieapplicationjetpackcompose.presentation.components
+package com.zaryabshakir.movieapplicationjetpackcompose.presentation.components.home
 
 import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.zaryabshakir.movieapplicationjetpackcompose.domain.model.Movie
+import com.zaryabshakir.movieapplicationjetpackcompose.presentation.components.Rating
+import com.zaryabshakir.movieapplicationjetpackcompose.presentation.theme.dph
+import com.zaryabshakir.movieapplicationjetpackcompose.presentation.theme.dpw
+import com.zaryabshakir.movieapplicationjetpackcompose.presentation.theme.nowShowingCardDimen
+import com.zaryabshakir.movieapplicationjetpackcompose.presentation.theme.nowShowingImage
 import com.zaryabshakir.movieapplicationjetpackcompose.util.TAG
 
 @Composable
@@ -28,22 +30,24 @@ fun NowShowingMovieCard(
 ) {
     Column(
         modifier = Modifier
-            .padding(start = 6.dp, end = 6.dp, bottom = 8.dp)
-            .width(150.dp)
-            .height(280.dp)
+            .padding(start = 16.dp)
+            .width(nowShowingCardDimen.width.dpw())
+            .height(nowShowingCardDimen.height.dph())
             .clickable {
-                Log.d(TAG, "NowShowingMovieCard: ${movie.toString()}")
                 onClick(movie.id)
             },
     ) {
         val imageUrl = preImageUrl + movie.posterPath
         Card(
-            shape = MaterialTheme.shapes.small,
+            shape = RoundedCornerShape(5.dp),
             modifier = Modifier
-                .height(200.dp)
-                .width(150.dp),
+                .height(nowShowingImage.height.dph())
+                .width(nowShowingImage.width.dpw()),
             elevation = 8.dp
         ) {
+            BoxWithConstraints {
+                Log.d(TAG, "NowShowingMovieCard: Image width $maxWidth Height $maxHeight ")
+            }
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(imageUrl)
@@ -55,11 +59,10 @@ fun NowShowingMovieCard(
         }
         Text(
             text = movie.originalTitle,
-            modifier = Modifier.padding(top = 6.dp, start = 2.dp, end = 2.dp),
-            style = TextStyle(
-                fontSize = 12.sp,
-                fontWeight = FontWeight(500)
-            )
+            modifier = Modifier.padding(top = 4.dp, start = 2.dp, end = 2.dp),
+            style = MaterialTheme.typography.h3,
+            color = MaterialTheme.colors.onPrimary,
+            maxLines = 2
         )
         Rating(
             rating = movie.voteAverage,
@@ -68,6 +71,4 @@ fun NowShowingMovieCard(
 
 
     }
-
-
 }
